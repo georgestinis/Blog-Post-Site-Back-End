6ad03b3a5a43ac847220ecc90f2abcb995fe9a30
georgestinis <george.stinis@yahoo.gr>
2021-02-05 12:52:53 +0200

    Add basic database structure

--- config/app.php
+++ config/app.php
@@ -161,7 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Overtrue\LaravelFollow\FollowServiceProvider::class,
+        
         /*
          * Package Service Providers...
          */
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/posts', [App\Http\Controllers\HomeController::class, 'posts'])->name('posts');
+Route::post('/like', [App\Http\Controllers\HomeController::class, 'LikePost'])->name('like');
